c7eae6425131517c0001ee556e6cc35f6bdfd34f
daniel <danielpaivaborges@outlook.com>
2023-11-24 07:28:50 -0300

    alteracoes de hoje

--- HTML/zoom_pageinfo.js
+++ HTML/zoom_pageinfo.js
@@ -1,4 +1,12 @@
 pageinfo = [[0,0,0,0,null],
+	[0,0,0,0,null],
+	[0,0,0,0,null],
+	[0,0,0,0,null],
+	[0,0,0,0,null],
 	[0,0,0,0,null]];
-pagedata = [ ["./git.html","GIT E GITHUB &#62; GIT","Git é um sistema de controle de versões distribuído, usado principalmente no desenvolvimento de software, mas pode ser usado para registrar o hist...",""],
+pagedata = [ ["./dicas-uteis.html","CSS &#62; DICAS UTEIS","1 - UTILIZANDO O COUNTER PARA NUMERAR ITENS DENTRO DA PÁGINA:",""],
+["./flex-direction.html","CSS &#62; FLEXBOX &#62; FLEX DIRECTION","FLEX DIRECTION é um sistema unidirecional que vai organizar os flex itens. Para utilizar é necessário setar a propriedade flex-direction no CSS.",""],
+["./flex-items.html","CSS &#62; FLEXBOX &#62; FLEX ITEMS","FLEX ITEMS são os filhos que estão contidos dentro do flex container que foi criado. ",""],
+["./flexbox.html","CSS &#62; FLEXBOX","Para utilizar flexbox precisamos ter uma div ou algo assim que possa conter todo o conteúdo para trabalhar, e este elemento deverá ser display:flex.",""],
+["./git.html","GIT E GITHUB &#62; MANUAL GIT E GIT HUB","Git é um sistema de controle de versões distribuído, usado principalmente no desenvolvimento de software, mas pode ser usado para registrar o hist...",""],
 ["./introduction.html","CSS","Enter topic text here.asdfasdfasdf asd f as df a sdf  asdf  as df asdf asdf asdf",""]];
